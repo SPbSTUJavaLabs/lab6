c2b21c3e569bcd728fa2e332ac192e2b53ad376b
marsofandrew <marsofandrew@mail.ru>
2018-03-16 20:31:15 +0300

    fix bugs
    1. add layout for DrawLayout
    2. Generate Points using width and height from special layout (p.1)

--- app/src/main/java/com/gopea_system/triangleuniversity/MainActivity.java
+++ app/src/main/java/com/gopea_system/triangleuniversity/MainActivity.java
@@ -16,8 +16,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn;
-    private ConstraintLayout layout;
-    private ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+    private LinearLayout layout;
+    private LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams
+            .MATCH_PARENT,
             LinearLayout.LayoutParams.MATCH_PARENT);
 
     private DrawView view;
@@ -27,14 +28,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = (Button) findViewById(R.id.btnCreate);
-        layout = (ConstraintLayout) findViewById(R.id.layout);
+        btn = findViewById(R.id.btnCreate);
+        layout = findViewById(R.id.layout);
         btn.setOnClickListener(v -> {
+            int width = layout.getWidth();
+            int height = layout.getHeight();
             if (view != null) {
                 layout.removeView(view);
             }
             Log.d("gopea.test", "Create are clicked");
-            Triangle triangle = generateTriangle();
+            Triangle triangle = ((width<0)||(height<0))?generateTriangle(400,600)
+                    :generateTriangle(width,height);
             view = new DrawView(this, triangle);
             layout.addView(view, params);
         });
@@ -46,11 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private Triangle generateTriangle() {
+    private Triangle generateTriangle(int maxWidthOfAreaForGenerate, int maxHeightOfAreaForGenerate) {
         Random rand = new Random();
         Point[] points = new Point[3];
         for (int i = 0; i < 3; i++) {
-            points[i] = new Point(rand.nextInt(1000), rand.nextInt(1500));
+            points[i] = new Point(rand.nextInt(maxWidthOfAreaForGenerate), rand.nextInt(maxHeightOfAreaForGenerate));
             Log.d("gopea.test", "point " + i + " = (" + points[i].getX() + ", " + points[i].getY() + ")");
         }
         Triangle triangle = new Triangle(points[0], points[1], points[2]);
